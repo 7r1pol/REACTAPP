1fd991bd45bda8cbb14e0f64fcc200c14732447f
7r1pol <pjk94@mail.ru>
2023-06-10 22:07:39 +0300

    news

--- src/Body/SearchForm.jsx
+++ src/Body/SearchForm.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { getArticles } from "../services/apiService";
 
 
-function SearchForm({ closeSideBar }) {
+function SearchForm({ closeSideBar, submitedData, setSubmitedData }) {
     const [articlesSortDisabled, setArticlesSortDisabled] = useState(false);
 
     const resultType = [
@@ -15,7 +16,7 @@ function SearchForm({ closeSideBar }) {
         "sourceExAggr",
         "authorAggr",
         "keywordAggr",
-        "locAggr", 
+        "locAggr",
         "conceptAggr",
         "conceptGraph",
         "categoryAggr",
@@ -32,25 +33,29 @@ function SearchForm({ closeSideBar }) {
         'sourceAlexaCountryRank',
         'socialScore',
         'facebookShares',
-        ];
-    const dataType = ["news","pr","blog",];
+    ];
+
+    const dataType = ["news", "pr", "blog",];
 
     const languages = [
-    {
-        label: 'English',
-        value: 'eng',
-    },
-    {
-        label: 'Eesti',
-        value: 'est',
-    },    
-    {
-        label: 'Russian',
-        value: 'rus',
-    },
-  
+        {
+            label: 'English',
+            value: 'eng',
+        },
+        {
+            label: 'Eesti',
+            value: 'est',
+        },
+        {
+            label: 'Русский',
+            value: 'rus',
+        },
+        {
+            label: 'Spanish',
+            value: 'spa',
+        },
     ];
-        
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -59,16 +64,20 @@ function SearchForm({ closeSideBar }) {
             resultType: event.target.resultType.value,
             articlesSortBy: event.target.articlesSortBy.value,
             dataType: [...event.target.dataType]
-            .filter((e) => e.checked)
-            .map((d) => d.value),
+                .filter((e) => e.checked)
+                .map((d) => d.value),
             lang: [...event.target.lang]
-            .filter((e) => e.selected)
-            .map((d) => d.value),
+                .filter((e) => e.selected)
+                .map((d) => d.value),
             dateStart: event.target.dateStart.value,
             dateEnd: event.target.dateEnd.value,
         };
 
+        setSubmitedData(data);
+
         console.log("data", data);
+
+        getArticles(data).then((res) => console.log("res", res));
 
         closeSideBar();
     };
@@ -80,73 +89,90 @@ function SearchForm({ closeSideBar }) {
             setArticlesSortDisabled(false);
         }
     };
+
     return (
         <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3">
                 <Form.Label>Keywords</Form.Label>
-                <Form.Control type="text" name="keyword" />
+                <Form.Control 
+                type="text"
+                name="keyword" 
+                defaultValue={submitedData?.keyword} 
+                />
             </Form.Group>
+
             <Form.Group className="mb-3">
                 <Form.Label>Result Type</Form.Label>
-                <Form.Select name="resultType" onChange={handleResultTypeChange}>
+                <Form.Select 
+                name="resultType" 
+                onChange={handleResultTypeChange} 
+                defaultValue={submitedData?.resultType}
+                >
                     {resultType.map((type) => (
                         <option value={type} key={type}>
                             {type}
-                            </option>
+                        </option>
                     ))}
-
                 </Form.Select>
             </Form.Group>
+
+
             <Form.Group className="mb-3">
                 <Form.Label>Articles Sort By</Form.Label>
-                <Form.Select name="articlesSortBy" disabled={articlesSortDisabled}>
+                <Form.Select name="articlesSortBy" disabled={articlesSortDisabled} defaultValue={submitedData?.articlesSortBy}>
                     {articlesSortBy.map((type) => (
                         <option value={type} key={type}>
                             {type}
-                            </option>
+                        </option>
                     ))}
-
                 </Form.Select>
             </Form.Group>
+
+
             <Form.Group className="mb-3">
                 <Form.Label>Data type</Form.Label>
                 {dataType.map((type) => (
                     <Form.Check
-                     type="checkbox"
-                      label={type}
-                       key={type}
+                        type="checkbox"
+                        label={type}
+                        key={type}
                         name="dataType"
-                         Value={type}
-                         />
-                        
-                        
+                        Value={type}
+                        defaultChecked={submitedData?.dataType.includes(type)}
+                    />
                 ))}
             </Form.Group>
 
-                    <Form.Group className="mb-3">
-                    <Form.Label>Language</Form.Label>
-                    <Form.Select name="lang" multiple>  
-                    {languages.map=(({value, label }) => (
+            <Form.Group className="mb-3">
+                <Form.Label>Language</Form.Label>
+                <Form.Select name="lang" multiple defaultValue={submitedData?.lang}>
+                    {languages.map(({ value, label }) => (
                         <option value={value} key={value}>
-                            {label}
-                            </option>
+                            {label},
+                        </option>
                     ))}
-                    </Form.Select>
-                    </Form.Group>
+                </Form.Select>
+            </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Date start</Form.Label>
-                        <Form.Control type="date" name="dateStart" />
-                    </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Date start</Form.Label>
+                <Form.Control 
+                type="date"
+                name="dateStart"
+                defaultValue={submitedData?.dateStart}/>
+            </Form.Group>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Date end</Form.Label>
-                        <Form.Control type="date" name="dateEnd" />
-                    </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Date end</Form.Label>
+                <Form.Control 
+                type="date" 
+                name="dateEnd"
+                defaultValue={submitedData?.dateEnd}/>
+            </Form.Group>
 
 
-            <Button variant="primary" type="submit">
-                 Close side bar
+            <Button variant="primary" type="submit" className="w-100">
+                Search
             </Button>
         </Form>
     );
